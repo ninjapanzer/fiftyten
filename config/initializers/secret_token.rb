d1f7e0c5bb350bee270af4effef44e3985104bb6
Paul Scarrone <paul.scarrone@gmail.com>
2013-01-21 12:33:46 -0500

    empty framework for 50/10 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiftyten::Application.config.secret_token = '3b458995315d660336ef6fc465b2e10912087a601ac85f7a1ffa428711fee262dfe93b1993b7ca4053ddb9a026796814b0f1121f12fc5d6fc57fc1c67e2bd9e9'
