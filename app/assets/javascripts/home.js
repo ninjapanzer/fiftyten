ccdfe50402c21880e94ff81af40f2ad2f81981b1
Paul Scarrone <paul.scarrone@gmail.com>
2013-02-02 22:42:19 -0500

    Get the timer front end working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,64 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var current_seconds = 0;
+
+String.prototype.toHHMMSS = function () {
+    sec_numb    = parseInt(this);
+    var hours   = Math.floor(sec_numb / 3600);
+    var minutes = Math.floor((sec_numb - (hours * 3600)) / 60);
+    var seconds = sec_numb - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+$(function(){
+	//Start Button for Clock
+	$("#start_clock").click(function(){
+		var clock_control = displayClock("clock", current_seconds);
+		
+		//Stop Button for Clock
+		$("#stop_clock").click(function(){
+			window.clearInterval(clock_control);
+			console.log("");
+			//Unbind when stop is hit
+			$("#stop_clock").unbind('click');
+		});
+		
+		//Reset Button for Clock
+		$("#clear_clock").click(function(){
+			//call stop to kill the timer and unbind the event
+			$("#stop_clock").click();
+			current_seconds = 0;
+			//reset display
+			document.getElementById("clock").innerHTML = (current_seconds+"").toHHMMSS();
+			//unbind own event
+			$("#clear_clock").unbind('click');
+		});
+	});
+});
+
+function displayClock(id, start_time){
+	var elapsed = typeof start_time !== 'undefined' ? start_time : 0;
+	var start = new Date().getTime() - (parseInt(elapsed)*1000);
+	var clock = window.setInterval(function(){
+    var time = (new Date().getTime() - start);
+    console.log(elapsed);
+    elapsed = Math.floor(time / 1000);
+    if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
+    current_seconds = elapsed.split(".")[0];
+    console.log(current_seconds);
+    document.getElementById(id).innerHTML = current_seconds.toHHMMSS();
+	}, 100);
+	return clock;
+}
+
+function makeClock(){
+	var minutes;
+	var seconds;
+
+}
